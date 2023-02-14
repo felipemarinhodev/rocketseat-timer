5c8c7b6986f63cc7cec25c40208163dcf5d03e9b
felipemarinhodev <felipmarinho@gmail.com>
2023-02-13 21:54:46 -0800

    adding custom components to button and inputs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,7 +3,10 @@ import {
   CountdownContainer,
   FormContainer,
   HomeContainer,
+  MinutesAmountInput,
   Separator,
+  StartCountdownButton,
+  TaskInput,
 } from './styles'
 
 export function Home() {
@@ -12,10 +15,14 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <input id="task" />
+          <TaskInput id="task" placeholder="Dê um nome para o seu projeto" />
 
           <label htmlFor="minuteAmount">durante</label>
-          <input type="number" id="minuteAmount" />
+          <MinutesAmountInput
+            type="number"
+            id="minuteAmount"
+            placeholder="00"
+          />
           <span>minutos</span>
         </FormContainer>
         <CountdownContainer>
@@ -26,10 +33,10 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <button type="submit">
+        <StartCountdownButton type="submit">
           <Play size={24} />
           Começar
-        </button>
+        </StartCountdownButton>
       </form>
     </HomeContainer>
   )
