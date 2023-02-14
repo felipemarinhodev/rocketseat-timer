a2a9052443c459c188d08175660d04ca356e0ad7
felipemarinhodev <felipmarinho@gmail.com>
2023-02-13 18:13:33 -0800

    adding ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import {ThemeProvider} from "styled-components";
+import {defaultTheme} from "./styles/themes/default";
+
 export function App() {
 
   return (
+    <ThemeProvider theme={defaultTheme}>
+
     <h1>lalala</h1>
+    </ThemeProvider>
   )
 }
