a4fc8cc3ed7bcaecc05d893cda2ce1557eff06e2
felipemarinhodev <felipmarinho@gmail.com>
2023-02-13 18:44:59 -0800

    configuring ESLint

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import {ThemeProvider} from "styled-components";
-import {defaultTheme} from "./styles/themes/default";
-import {GlobalStyle} from "./styles/global";
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
 export function App() {
-
   return (
     <ThemeProvider theme={defaultTheme}>
-
-    <h1>lalala</h1>
+      <h1>lalala</h1>
       <GlobalStyle />
     </ThemeProvider>
   )
